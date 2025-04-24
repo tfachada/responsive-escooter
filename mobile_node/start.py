cad2c7991612fe7300fd02a0cdb8ec5d5b3ac538
Tomás Fachada <tomas.fachada@tecnico.ulisboa.pt>
2025-04-24 15:15:32 +0100

    init code tweaks

--- mobile_node/start.py
+++
@@ -1,8 +0,0 @@
-import os
-import subprocess
-
-source_root = f'export HDTN_SOURCE_ROOT=~/HDTN'
-
-subprocess.run("sudo pigpiod", shell=True)
-os.environ['HDTN_SOURCE_ROOT'] = '~/HDTN'
-print(f"Defined source_root: {os.getenv('HDTN_SOURCE_ROOT')}")
